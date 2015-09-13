a0d936aadbdd9bc38c253bf722670f0108fd8427
Aaron Bedra <aaron@aaronbedra.com>
2015-09-13 14:58:21 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # repsheet
-The Repsheet command line utility
+
+The Repsheet command line utility. If you are looking for the original Repsheet project, please visit the [NGINX](https://github.com/repsheet/repsheet-nginx) or [Apache](https://github.com/repsheet/repsheet-apache) modules. They contain implementations for each webserver, but share a common [core library](https://github.com/repsheet/librepsheet).
+
+## Installation
+
+```
+$ go get github.com/repsheet/repsheet
+```
+
+## Usage
+
+```
+$ repsheet -blacklist=1.1.1.1
+Blacklisting 1.1.1.1
+$ repsheet -whitelist=2.2.2.2
+Whitelisting 2.2.2.2
+$ repsheet -mark=3.3.3.3
+Marking 3.3.3.3
+$ repsheet -list
+Whitelisted Actors
+  2.2.2.2:repsheet:ip:whitelisted
+Blacklisted Actors
+  1.1.1.1:repsheet:ip:blacklisted
+Marked Actors
+  3.3.3.3:repsheet:ip:marked
+```
---
+++ repsheet.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+        "flag"
+        "fmt"
+        "time"
+        "github.com/fzzy/radix/redis"
+)
+
+func main() {
+        listPtr := flag.Bool("list", false, "Show the contents of Repsheet")
+        blacklistPtr := flag.String("blacklist", "", "Blacklist an actor")
+        whitelistPtr := flag.String("whitelist", "", "Whitelist an actor")
+        markPtr := flag.String("mark", "", "Mark an actor")
+        reasonPtr := flag.String("reason", "", "Reason for the action")
+
+        flag.Parse()
+
+        conn, err := redis.DialTimeout("tcp", "127.0.0.1:6379", time.Duration(10) * time.Second)
+        if err != nil {
+                panic("Error connecting to Redis")
+        }
+
+        if (*listPtr == true) {
+		whitelisted := conn.Cmd("KEYS", "*:repsheet:*:whitelisted")
+		if len(whitelisted.Elems) > 0 {
+			fmt.Println("Whitelisted Actors")
+		}
+		for i := 0; i < len(whitelisted.Elems); i++ {
+			fmt.Printf("  %s\n", whitelisted.Elems[i])
+		}
+
+		blacklisted := conn.Cmd("KEYS", "*:repsheet:*:blacklisted")
+		if len(blacklisted.Elems) > 0 {
+			fmt.Println("Blacklisted Actors")
+		}
+		for i := 0; i < len(blacklisted.Elems); i++ {
+			fmt.Printf("  %s\n", blacklisted.Elems[i])
+		}
+
+		marked := conn.Cmd("KEYS", "*:repsheet:*:marked")
+		if len(marked.Elems) > 0 {
+			fmt.Println("Marked Actors")
+		}
+		for i := 0; i < len(marked.Elems); i++ {
+			fmt.Printf("  %s\n", marked.Elems[i])
+		}
+        }
+
+        if (*blacklistPtr != "") {
+                fmt.Println("Blacklisting", *blacklistPtr)
+                command := fmt.Sprintf("%s:repsheet:ip:blacklisted", *blacklistPtr)
+
+                if (*reasonPtr != "") {
+                        conn.Cmd("SET", command, *reasonPtr)
+                } else {
+                        conn.Cmd("SET", command, "true")
+                }
+
+        }
+
+        if (*whitelistPtr != "") {
+                fmt.Println("Whitelisting", *whitelistPtr)
+                command := fmt.Sprintf("%s:repsheet:ip:whitelisted", *whitelistPtr)
+
+                if (*reasonPtr != "") {
+                        conn.Cmd("SET", command, *reasonPtr)
+                } else {
+                        conn.Cmd("SET", command, "true")
+                }
+        }
+
+        if (*markPtr != "") {
+                fmt.Println("Marking", *markPtr)
+                command := fmt.Sprintf("%s:repsheet:ip:marked", *markPtr)
+
+                if (*reasonPtr != "") {
+                        conn.Cmd("SET", command, *reasonPtr)
+                } else {
+                        conn.Cmd("SET", command, "true")
+                }
+        }
+}
