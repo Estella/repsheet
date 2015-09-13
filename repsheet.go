6c48b300bde9b0dc76b3c2d333a50750bd468e97
Aaron Bedra <aaron@aaronbedra.com>
2015-09-13 18:20:45 -0500

    Factor out list printing

--- repsheet.go
+++ repsheet.go
@@ -1,83 +1,77 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/repsheet/repsheet/Godeps/_workspace/src/github.com/fzzy/radix/redis"
-	"time"
+        "flag"
+        "fmt"
+        "github.com/repsheet/repsheet/Godeps/_workspace/src/github.com/fzzy/radix/redis"
+        "time"
 )
 
+func printList(actorType string, list *redis.Reply) {
+        if len(list.Elems) > 0 {
+                fmt.Printf("%s Actors\n", actorType)
+        }
+        for i := 0; i < len(list.Elems); i++ {
+                fmt.Printf("  %s\n", list.Elems[i])
+        }
+}
+
 func main() {
-	listPtr := flag.Bool("list", false, "Show the contents of Repsheet")
-	blacklistPtr := flag.String("blacklist", "", "Blacklist an actor")
-	whitelistPtr := flag.String("whitelist", "", "Whitelist an actor")
-	markPtr := flag.String("mark", "", "Mark an actor")
-	reasonPtr := flag.String("reason", "", "Reason for the action")
+        listPtr := flag.Bool("list", false, "Show the contents of Repsheet")
+        blacklistPtr := flag.String("blacklist", "", "Blacklist an actor")
+        whitelistPtr := flag.String("whitelist", "", "Whitelist an actor")
+        markPtr := flag.String("mark", "", "Mark an actor")
+        reasonPtr := flag.String("reason", "", "Reason for the action")
 
-	flag.Parse()
+        flag.Parse()
 
-	conn, err := redis.DialTimeout("tcp", "127.0.0.1:6379", time.Duration(10)*time.Second)
-	if err != nil {
-		panic("Error connecting to Redis")
-	}
+        conn, err := redis.DialTimeout("tcp", "127.0.0.1:6379", time.Duration(10)*time.Second)
+        if err != nil {
+                panic("Error connecting to Redis")
+        }
 
-	if *listPtr == true {
-		whitelisted := conn.Cmd("KEYS", "*:repsheet:*:whitelisted")
-		if len(whitelisted.Elems) > 0 {
-			fmt.Println("Whitelisted Actors")
-		}
-		for i := 0; i < len(whitelisted.Elems); i++ {
-			fmt.Printf("  %s\n", whitelisted.Elems[i])
-		}
+        if *listPtr == true {
+                whitelisted := conn.Cmd("KEYS", "*:repsheet:*:whitelisted")
+		printList("Whitelisted", whitelisted)
 
-		blacklisted := conn.Cmd("KEYS", "*:repsheet:*:blacklisted")
-		if len(blacklisted.Elems) > 0 {
-			fmt.Println("Blacklisted Actors")
-		}
-		for i := 0; i < len(blacklisted.Elems); i++ {
-			fmt.Printf("  %s\n", blacklisted.Elems[i])
-		}
+                blacklisted := conn.Cmd("KEYS", "*:repsheet:*:blacklisted")
+		printList("Blacklisted", blacklisted)
 
-		marked := conn.Cmd("KEYS", "*:repsheet:*:marked")
-		if len(marked.Elems) > 0 {
-			fmt.Println("Marked Actors")
-		}
-		for i := 0; i < len(marked.Elems); i++ {
-			fmt.Printf("  %s\n", marked.Elems[i])
-		}
-	}
+                marked := conn.Cmd("KEYS", "*:repsheet:*:marked")
+		printList("Marked", marked)
+        }
 
-	if *blacklistPtr != "" {
-		fmt.Println("Blacklisting", *blacklistPtr)
-		command := fmt.Sprintf("%s:repsheet:ip:blacklisted", *blacklistPtr)
+        if *blacklistPtr != "" {
+                fmt.Println("Blacklisting", *blacklistPtr)
+                command := fmt.Sprintf("%s:repsheet:ip:blacklisted", *blacklistPtr)
 
-		if *reasonPtr != "" {
-			conn.Cmd("SET", command, *reasonPtr)
-		} else {
-			conn.Cmd("SET", command, "true")
-		}
+                if *reasonPtr != "" {
+                        conn.Cmd("SET", command, *reasonPtr)
+                } else {
+                        conn.Cmd("SET", command, "true")
+                }
 
-	}
+        }
 
-	if *whitelistPtr != "" {
-		fmt.Println("Whitelisting", *whitelistPtr)
-		command := fmt.Sprintf("%s:repsheet:ip:whitelisted", *whitelistPtr)
+        if *whitelistPtr != "" {
+                fmt.Println("Whitelisting", *whitelistPtr)
+                command := fmt.Sprintf("%s:repsheet:ip:whitelisted", *whitelistPtr)
 
-		if *reasonPtr != "" {
-			conn.Cmd("SET", command, *reasonPtr)
-		} else {
-			conn.Cmd("SET", command, "true")
-		}
-	}
+                if *reasonPtr != "" {
+                        conn.Cmd("SET", command, *reasonPtr)
+                } else {
+                        conn.Cmd("SET", command, "true")
+                }
+        }
 
-	if *markPtr != "" {
-		fmt.Println("Marking", *markPtr)
-		command := fmt.Sprintf("%s:repsheet:ip:marked", *markPtr)
+        if *markPtr != "" {
+                fmt.Println("Marking", *markPtr)
+                command := fmt.Sprintf("%s:repsheet:ip:marked", *markPtr)
 
-		if *reasonPtr != "" {
-			conn.Cmd("SET", command, *reasonPtr)
-		} else {
-			conn.Cmd("SET", command, "true")
-		}
-	}
+                if *reasonPtr != "" {
+                        conn.Cmd("SET", command, *reasonPtr)
+                } else {
+                        conn.Cmd("SET", command, "true")
+                }
+        }
 }
