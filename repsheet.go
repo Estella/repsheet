897b093c4e5d54943c9d58126fa78a3f9f18bddb
Aaron Bedra <aaron@aaronbedra.com>
2015-09-15 23:38:07 -0500

    Only ask Redis once for full list

--- repsheet.go
+++ repsheet.go
@@ -6,52 +6,82 @@ import (
         "github.com/repsheet/repsheet/Godeps/_workspace/src/github.com/fzzy/radix/redis"
         "time"
         "os"
-        "syscall"
+	"syscall"
+	"strings"
 )
 
 type StatusType uint8
 
 const (
-	UnitType StatusType = iota
-	Blacklisted
-	Whitelisted
-	Marked
-	OK
+        UnitType StatusType = iota
+        Blacklisted
+        Whitelisted
+        Marked
+        OK
 )
 
 type Status struct {
-	Type StatusType
-	reason string
-	actor string
+        Type StatusType
+        reason string
+        actor string
 }
 
 func printStatus(status *Status) {
-	var statusStr string
-	switch status.Type {
-	case Blacklisted:
-		statusStr = "blacklisted"
-	case Whitelisted:
-		statusStr = "whitelisted"
-	case Marked:
-		statusStr = "marked"
-	default:
-		statusStr = "OK"
-	}
+        var statusStr string
+        switch status.Type {
+        case Blacklisted:
+                statusStr = "blacklisted"
+        case Whitelisted:
+                statusStr = "whitelisted"
+        case Marked:
+                statusStr = "marked"
+        default:
+                statusStr = "OK"
+        }
 
-	if status.reason != "" {
-		fmt.Printf("%s is %s. Reason: %s\n", status.actor, statusStr, status.reason)
-	} else {
-		fmt.Printf("%s is %s\n", status.actor, statusStr)
-	}
+        if status.reason != "" {
+                fmt.Printf("%s is %s. Reason: %s\n", status.actor, statusStr, status.reason)
+        } else {
+                fmt.Printf("%s is %s\n", status.actor, statusStr)
+        }
 }
 
-func printList(actorType string, list *redis.Reply) {
-        if len(list.Elems) > 0 {
-                fmt.Printf("%s Actors\n", actorType)
+func printLists(lists map[string][]string) {
+        for k, v := range lists {
+                fmt.Printf("%s actors\n", k)
+                for i := 0; i < len(v); i++ {
+                        fmt.Printf("  %s\n", v[i])
+                }
         }
-        for i := 0; i < len(list.Elems); i++ {
-                fmt.Printf("  %s\n", list.Elems[i])
+}
+
+func list(connection *redis.Client) {
+        var blacklisted []string
+        var whitelisted []string
+        var marked []string
+
+        repsheet := connection.Cmd("KEYS", "*:repsheet:*:*")
+        for i := 0; i < len(repsheet.Elems); i++ {
+                value, _ := repsheet.Elems[i].Str()
+                parts := strings.Split(value, ":")
+                repsheetType := parts[len(parts)-1]
+
+                switch repsheetType {
+                case "blacklisted":
+                        blacklisted = append(blacklisted, parts[0])
+                case "whitelisted":
+                        whitelisted = append(whitelisted, parts[0])
+                case "marked":
+                        marked = append(marked, parts[0])
+                }
         }
+
+        var m = make(map[string][]string)
+        m["blacklisted"] = blacklisted
+        m["whitelisted"] = whitelisted
+        m["marked"] = marked
+
+        printLists(m)
 }
 
 func addToList(connection *redis.Client, list string, actor string, reason string) {
@@ -65,7 +95,7 @@ func addToList(connection *redis.Client, list string, actor string, reason strin
 }
 
 func connect(host string, port int, timeout int) *redis.Client {
-	connectionString := fmt.Sprintf("%s:%d", host, port)
+        connectionString := fmt.Sprintf("%s:%d", host, port)
         conn, err := redis.DialTimeout("tcp", connectionString, time.Duration(timeout)*time.Second)
 
         if err != nil {
@@ -73,63 +103,56 @@ func connect(host string, port int, timeout int) *redis.Client {
                 os.Exit(int(syscall.ECONNREFUSED))
         }
 
-	return conn;
+        return conn;
 }
 
 func status(connection *redis.Client, actor string) *Status {
-	blacklistedString := fmt.Sprintf("%s:repsheet:ip:blacklisted", actor)
-	whitelistedString := fmt.Sprintf("%s:repsheet:ip:whitelisted", actor)
-	markedString := fmt.Sprintf("%s:repsheet:ip:marked", actor)
-	connection.Cmd("MULTI")
-	connection.Cmd("GET", whitelistedString)
-	connection.Cmd("GET", blacklistedString)
-	connection.Cmd("GET", markedString)
-	reply := connection.Cmd("EXEC")
+        blacklistedString := fmt.Sprintf("%s:repsheet:ip:blacklisted", actor)
+        whitelistedString := fmt.Sprintf("%s:repsheet:ip:whitelisted", actor)
+        markedString := fmt.Sprintf("%s:repsheet:ip:marked", actor)
+        connection.Cmd("MULTI")
+        connection.Cmd("GET", whitelistedString)
+        connection.Cmd("GET", blacklistedString)
+        connection.Cmd("GET", markedString)
+        reply := connection.Cmd("EXEC")
 
-	if reply.Elems[0].Type != redis.NilReply {
-		str, _ := reply.Elems[0].Str()
-		return &Status{Type: Whitelisted, reason: str, actor: actor}
-	} else if reply.Elems[1].Type != redis.NilReply {
-		str, _ := reply.Elems[1].Str()
-		return &Status{Type: Blacklisted, reason: str, actor: actor}
-	} else if reply.Elems[2].Type != redis.NilReply {
-		str, _ := reply.Elems[1].Str()
-		return &Status{Type: Marked, reason: str, actor: actor}
-	} else {
-		return &Status{Type: OK, reason: "", actor: actor}
-	}
+        if reply.Elems[0].Type != redis.NilReply {
+                str, _ := reply.Elems[0].Str()
+                return &Status{Type: Whitelisted, reason: str, actor: actor}
+        } else if reply.Elems[1].Type != redis.NilReply {
+                str, _ := reply.Elems[1].Str()
+                return &Status{Type: Blacklisted, reason: str, actor: actor}
+        } else if reply.Elems[2].Type != redis.NilReply {
+                str, _ := reply.Elems[1].Str()
+                return &Status{Type: Marked, reason: str, actor: actor}
+        } else {
+                return &Status{Type: OK, reason: "", actor: actor}
+        }
 }
 
 func main() {
         listPtr := flag.Bool("list", false, "Show the contents of Repsheet")
-	statusPtr := flag.String("status", "", "Get the status of an actor")
+        statusPtr := flag.String("status", "", "Get the status of an actor")
         blacklistPtr := flag.String("blacklist", "", "Blacklist an actor")
         whitelistPtr := flag.String("whitelist", "", "Whitelist an actor")
         markPtr := flag.String("mark", "", "Mark an actor")
         reasonPtr := flag.String("reason", "", "Reason for the action")
-	hostPtr := flag.String("host", "localhost", "Redis host")
-	portPtr := flag.Int("port", 6379, "Redis port")
-	timeoutPtr := flag.Int("timeout", 10, "Redis connection timeout")
+        hostPtr := flag.String("host", "localhost", "Redis host")
+        portPtr := flag.Int("port", 6379, "Redis port")
+        timeoutPtr := flag.Int("timeout", 10, "Redis connection timeout")
 
         flag.Parse()
 
-	connection := connect(*hostPtr, *portPtr, *timeoutPtr)
+        connection := connect(*hostPtr, *portPtr, *timeoutPtr)
 
         if *listPtr == true {
-                whitelisted := connection.Cmd("KEYS", "*:repsheet:*:whitelisted")
-                printList("Whitelisted", whitelisted)
-
-                blacklisted := connection.Cmd("KEYS", "*:repsheet:*:blacklisted")
-                printList("Blacklisted", blacklisted)
-
-                marked := connection.Cmd("KEYS", "*:repsheet:*:marked")
-                printList("Marked", marked)
+                list(connection)
         }
 
-	if *statusPtr != "" {
-		status := status(connection, *statusPtr)
-		printStatus(status)
-	}
+        if *statusPtr != "" {
+                status := status(connection, *statusPtr)
+                printStatus(status)
+        }
 
         if *blacklistPtr != "" {
                 addToList(connection, "blacklist", *blacklistPtr, *reasonPtr)
